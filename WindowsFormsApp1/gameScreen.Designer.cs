a3d6f1e342f7d96298ce5b16b84a6f89b07dba6e
Adrian Racho <adrirach172@ed.amdsb.ca>
2022-03-28 09:56:21 -0400

    pog

--- WindowsFormsApp1/gameScreen.Designer.cs
+++ WindowsFormsApp1/gameScreen.Designer.cs
@@ -30,38 +30,24 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTImer = new System.Windows.Forms.Timer(this.components);
-            this.fortnite = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.fortnite)).BeginInit();
             this.SuspendLayout();
             // 
             // gameTImer
             // 
             this.gameTImer.Enabled = true;
-            this.gameTImer.Interval = 10;
+            this.gameTImer.Interval = 20;
             this.gameTImer.Tick += new System.EventHandler(this.gameTImer_Tick);
-            // 
-            // fortnite
-            // 
-            this.fortnite.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.fortnitereals;
-            this.fortnite.Location = new System.Drawing.Point(142, 354);
-            this.fortnite.Name = "fortnite";
-            this.fortnite.Size = new System.Drawing.Size(34, 22);
-            this.fortnite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.fortnite.TabIndex = 0;
-            this.fortnite.TabStop = false;
             // 
             // gameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.fortnite);
             this.DoubleBuffered = true;
             this.Name = "gameScreen";
             this.Size = new System.Drawing.Size(600, 600);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.gameScreen_Paint);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameScreen_KeyUp);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.gameScreen_PreviewKeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.fortnite)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +55,5 @@
         #endregion
 
         private System.Windows.Forms.Timer gameTImer;
-        private System.Windows.Forms.PictureBox fortnite;
     }
 }
--- WindowsFormsApp1/gameScreen.cs
+++ WindowsFormsApp1/gameScreen.cs
@@ -13,13 +13,13 @@ namespace WindowsFormsApp1
     public partial class gameScreen : UserControl
     {
 
-        Player players;
+        
         bool wkey = false;
         bool akey = false;
         bool skey = false;
         bool dkey = false;
-        Size screenSize;
-        int speeds = 6;
+                  
+
         int sidey = 200;
         int sidex = 200;
         int sideHeight = 1;
@@ -52,36 +52,27 @@ namespace WindowsFormsApp1
         int sidex8 = 120;
         int sideHeight8 = 1;
         int sideWidth8 = 80;
-        int ySpeed = 1;
-        int xspeed = 1;
-        int x;
-        int y;
-     int   size = 50;
+     
+        int playerx = 142;
+        int playery = 354;
+   
+        int playerspeedy = 5;
+        int playerspeedx = 5;
+        int playerWidth = 30;
+        int playerHeight = 30;
         public gameScreen()
         {
             InitializeComponent();
-            InitializeGame();
+           
         }
-        public void InitializeGame()
-        {
-            screenSize = new Size(this.Width, this.Height);
-
-            int x = 142; ;
-            int y = 354;
-
-
-            players = new Player(x, y);
-            
-
-        }
-    
+      
 
         private void gameScreen_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillRectangle(Brushes.Pink, 120, 340, 80, 60);
             e.Graphics.FillRectangle(Brushes.Blue, 200, 200, 200, 200);
             e.Graphics.FillRectangle(Brushes.Green, 275, 150, 50, 50);
-            e.Graphics.FillRectangle(Brushes.Blue, sidex, sidey, sideWidth, sideHeight);
+            e.Graphics.FillRectangle(Brushes.Red, sidex, sidey, sideWidth, sideHeight);
             e.Graphics.FillRectangle(Brushes.Blue, sidex2, sidey2, sideWidth2, sideHeight2);
             e.Graphics.FillRectangle(Brushes.Pink, sidex3, sidey3, sideWidth3, sideHeight3);
             e.Graphics.FillRectangle(Brushes.Blue, sidex4, sidey4, sideWidth4, sideHeight4);
@@ -89,138 +80,89 @@ namespace WindowsFormsApp1
             e.Graphics.FillRectangle(Brushes.Blue, sidex6, sidey6, sideWidth6, sideHeight6);
             e.Graphics.FillRectangle(Brushes.Pink, sidex7, sidey7, sideWidth7, sideHeight7);
             e.Graphics.FillRectangle(Brushes.Pink, sidex8, sidey8, sideWidth8, sideHeight8);
-            e.Graphics.FillRectangle(Brushes.Black, players.x, players.y, players.width,players.height);
+            e.Graphics.FillRectangle(Brushes.Black, playerx, playery, playerWidth,playerHeight);
         }
 
         private void gameTImer_Tick(object sender, EventArgs e)
         {
-            if (akey == true && fortnite.Left > 0)
-            {
-                fortnite.Left -= speeds;
-            }
 
-            if (dkey == true && fortnite.Left < 600)
-            {
-                fortnite.Left += speeds;
 
-            }
-
-            if (skey == true&& fortnite.Top >0)
+            if (akey == true )
             {
-                fortnite.Top += speeds;
-            }
-
-            if (wkey == true && fortnite.Top < 600)
-            {
-               fortnite.Top -= speeds;
-            }
-
-            if (akey == true)
-            {
-                players.Move("left", screenSize);
+                playerx -= playerspeedx;
             }
 
             if (dkey == true)
             {
-                players.Move("right", screenSize);
+                playerx += playerspeedx;
+
+            }
+                if (wkey == true)
+                {
+                    playery -= playerspeedy;
+                }
+
+                if (skey == true)
+                {
+
+                playery += playerspeedy;
+
             }
 
-            if (wkey == true)
+                Refresh();
+
+                Rectangle playerRec = new Rectangle(playerx, playery, sidex, sidey);
+            Rectangle sideRec = new Rectangle(playerx, playery, sidex, sidey);
+
+            if (playerRec.IntersectsWith(sideRec))
             {
-                players.Move("up", screenSize);
+               
             }
 
-            if (dkey == true)
-            {
-                players.Move("down", screenSize);
-            }
-           
-            Refresh();
-
-            Rectangle playerRec = new Rectangle  (x, y, sidex, sidey);
-            Rectangle sideRec = new Rectangle(players.x, players.y, players.width, players.height);
-
-            if (sideRec.IntersectsWith(playerRec))
-            {
-             
-
-            }
-         
 
 
         }
-
+        
 
         private void gameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
             switch (e.KeyCode)
             {
-                case Keys.W:
-                    wkey = true;
-                    break;
-                case Keys.S:
-                    skey = true;
-                    break;
                 case Keys.A:
                     akey = true;
                     break;
                 case Keys.D:
                     dkey = true;
                     break;
+                case Keys.W:
+                    wkey = true;
+                    break;
+                case Keys.S:
+                    skey = true;
+                    break;
             }
 
-            if (e.KeyCode == Keys.Up)
-            {
-                wkey = true;
-            }
-            if (e.KeyCode == Keys.Left)
-            {
-                akey = true;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                skey = true;
-            }
-            if (e.KeyCode == Keys.Right)
-            {
-                dkey = true;
-            }
 
         }
 
         private void gameScreen_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Up)
-            {
-                wkey = false;
-            }
-            if (e.KeyCode == Keys.Left)
-            {
-                akey = false;
-            }
-            if (e.KeyCode == Keys.Down)
-            {
-                skey = false;
-            }
-            if (e.KeyCode == Keys.Right)
-            {
-                dkey = false;
-            }
             switch (e.KeyCode)
             {
-                case Keys.W:
-                    wkey = false;
-                    break;
-                case Keys.S:
-                    skey = false;
-                    break;
                 case Keys.A:
                     akey = false;
                     break;
                 case Keys.D:
                     dkey = false;
                     break;
+                case Keys.W:
+                    wkey = false;
+                    break;
+                case Keys.S:
+                    skey = false;
+                    break;
             }
+
         }
     }
 }
